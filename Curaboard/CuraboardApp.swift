ab507796d8ec12b2dce313133dc593525a208cde
nobletp001 <ojotemitopeidnoble10@gmail.com>
2025-04-19 18:50:47 +0100

    Initial Commit

---
+++ Curaboard/CuraboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuraboardApp.swift
+//  Curaboard
+//
+//  Created by machine on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct CuraboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
